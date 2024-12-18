e8e79a3ed89add74880462a6aa8de62cecbeecf6
Dor Meiri <dormeiri@gmail.com>
2024-12-18 10:23:10 +0200

    CodeCompanion

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -1,3 +1,22 @@
 return {
 	{ "github/copilot.vim" },
+	{
+		"olimorris/codecompanion.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-treesitter/nvim-treesitter",
+		},
+		config = function()
+			require("codecompanion").setup({
+				strategies = {
+					chat = {
+						adapter = "copilot",
+					},
+					inline = {
+						adapter = "copilot",
+					},
+				},
+			})
+		end,
+	},
 }
