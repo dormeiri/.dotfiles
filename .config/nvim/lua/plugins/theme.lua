a464daed6f414352f1590510209539b18b6ba59e
Dor Meiri <dormeiri@gmail.com>
2025-01-22 07:33:23 +0200

    chore: update lazy-lock.json and theme.lua
    - Update commit hashes for various plugins in lazy-lock.json
    - Change catppuccin plugin source to dormeiri/catppuccin-nvim in theme.lua
    - Add gitgraph and harpoon integrations in theme.lua

--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -1,17 +1,19 @@
 return {
 	{
-		"catppuccin/nvim",
+		"dormeiri/catppuccin-nvim",
 		name = "catppuccin",
 		lazy = false,
 		priority = 1000,
 		opts = {
 			integrations = {
 				diffview = true,
+				gitgraph = true,
 				mason = true,
 				noice = true,
 				copilot_vim = true,
 				notify = true,
 				which_key = true,
+				harpoon = true,
 			},
 		},
 	},
